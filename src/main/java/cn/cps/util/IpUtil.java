8d9cf5bfd32b66b8888a98ac758e112cfe455b14
Cps <1402333029@qq.com>
2019-07-28 17:54:09 +0800

    添加IP和HttpUtil工具类

--- src/main/java/cn/cps/util/IpUtil.java
+++ src/main/java/cn/cps/util/IpUtil.java
@@ -1,145 +1,213 @@
 package cn.cps.util;
 
-import com.github.pagehelper.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
-import java.net.NetworkInterface;
 import java.net.UnknownHostException;
-import java.util.Enumeration;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
- * Created with IntelliJ IDEA.
- * Description:
- *  IP操作工具类
- * @author LErry.li
- * Date: 2018-06-16
- * Time: 16:59
+ * 获取IP方法
+ * 
+ * @author ruoyi
  */
-public class IpUtil {
+public class IpUtil
+{
 
-    private IpUtil() {
-
-    }
-
-    private static final Logger logger = LoggerFactory.getLogger(IpUtil.class);
-
-    private static final String IP_PATTERN = "^(?:(?:[01]?\\d{1,2}|2[0-4]\\d|25[0-5])\\.){3}(?:[01]?\\d{1,2}|2[0-4]\\d|25[0-5])\\b";
-
-    private static final String UNKNOWN = "unknown";
-
-    private static final String LOCAL_IP = "127.0.0.1";
+    private static final Logger log = LoggerFactory.getLogger(IpUtil.class);
+    public static final String IP_URL = "http://ip.taobao.com/service/getIpInfo.php";
 
     /**
-     * 获取请求中的ip地址：过了多级反向代理，获取的ip不是唯一的，二是包含中间代理层ip
-     * @param request
-     * @return 可能有多个，例如：192.168.1.110， 192.168.1.120
-     */
-    public static String getIpAddr(HttpServletRequest request) {
-        String ip = LOCAL_IP;
-        if (request != null) {
-            ip = request.getHeader("x-forwarded-for");
-            if (StringUtil.isEmpty(ip) || UNKNOWN.equalsIgnoreCase(ip)) {
-                ip = request.getHeader("Proxy-Client-IP");
-            }
-
-            if (StringUtil.isEmpty(ip) || UNKNOWN.equalsIgnoreCase(ip)) {
-                ip = request.getHeader("WL-Proxy-Client-IP");
-            }
-
-            if (StringUtil.isEmpty(ip) || UNKNOWN.equalsIgnoreCase(ip)) {
-                ip = request.getRemoteAddr();
-            }
-        }
-        return ip;
-
-    }
-
-    /**
-     * 获取客户端请求中的真实的ip地址
-     *  获取客户端的IP地址的方法是：request.getRemoteAddr()，这种方法在大部分情况下都是有效的。
-     *  但是在通过了Apache，Squid等反向代理软件就不能获取到客户端的真实IP地址。而且，如果通过了多级反向代理的话，X-Forwarded-For的值并不止一个，
-     *  而是一串ip值，例如：192.168.1.110， 192.168.1.120， 192.168.1.130， 192.168.1.100。其中第一个192.168.1.110才是用户真实的ip
-     * @param request
-     * @return
-     */
-    public static String getRealIp(HttpServletRequest request) {
-        String ip = LOCAL_IP;
-        if (request == null) {
-            return ip;
-        }
-        ip = request.getHeader("x-forwarded-for");
-        ip = getIp(request,ip);
-        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
-            if (LOCAL_IP.equals(ip) || "0:0:0:0:0:0:0:1".equals(ip)) {
-                //根据网卡取本机配置的IP
-                InetAddress inet = null;
-                try {
-                    inet = InetAddress.getLocalHost();
-                    ip = inet.getHostAddress();
-                } catch (UnknownHostException e) {
-                    logger.error("getRealIp occurs error, caused by: ", e);
-                }
-            }
-        }
-        String ch = ",";
-        if (ip != null && ip.contains(ch)) {
-                ip = ip.substring(0, ip.indexOf(ch));
-        }
-        return ip;
-    }
-
-    /**
-     * 通过各种方式获取IP
-     * @param request
+     * 根据IP获取地址
      * @param ip
      * @return
      */
-    private static String getIp(HttpServletRequest request, String ip){
-        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
+    public static String getRealAddressByIP(String ip)
+    {
+        String address = "XX XX";
+        // 内网不查询
+        if (IpUtil.internalIp(ip))
+        {
+            return "内网IP";
+        }
+        String rspStr = HttpUtil.sendPost(IP_URL, "ip=" + ip);
+        if (rspStr.isEmpty())
+        {
+            log.error("获取地理位置异常 {}", ip);
+            return address;
+        }
+        return address;
+    }
+
+    public static String getIpAddr(HttpServletRequest request)
+    {
+        if (request == null)
+        {
+            return "unknown";
+        }
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
+        {
             ip = request.getHeader("Proxy-Client-IP");
         }
-        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
+        {
+            ip = request.getHeader("X-Forwarded-For");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
+        {
             ip = request.getHeader("WL-Proxy-Client-IP");
         }
-        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_CLIENT_IP");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
+        {
+            ip = request.getHeader("X-Real-IP");
         }
-        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
+        {
+            ip = request.getRemoteAddr();
         }
-        return ip;
+
+        return "0:0:0:0:0:0:0:1".equals(ip) ? "127.0.0.1" : ip;
+    }
+
+    public static boolean internalIp(String ip)
+    {
+        byte[] addr = textToNumericFormatV4(ip);
+        return internalIp(addr) || "127.0.0.1".equals(ip);
+    }
+
+    private static boolean internalIp(byte[] addr)
+    {
+        final byte b0 = addr[0];
+        final byte b1 = addr[1];
+        // 10.x.x.x/8
+        final byte SECTION_1 = 0x0A;
+        // 172.16.x.x/12
+        final byte SECTION_2 = (byte) 0xAC;
+        final byte SECTION_3 = (byte) 0x10;
+        final byte SECTION_4 = (byte) 0x1F;
+        // 192.168.x.x/16
+        final byte SECTION_5 = (byte) 0xC0;
+        final byte SECTION_6 = (byte) 0xA8;
+        switch (b0)
+        {
+            case SECTION_1:
+                return true;
+            case SECTION_2:
+                if (b1 >= SECTION_3 && b1 <= SECTION_4)
+                {
+                    return true;
+                }
+            case SECTION_5:
+                switch (b1)
+                {
+                    case SECTION_6:
+                        return true;
+                }
+            default:
+                return false;
+        }
     }
 
     /**
-     * 获取服务器IP
+     * 将IPv4地址转换成字节
+     * 
+     * @param text IPv4地址
+     * @return byte 字节
      */
-    public static String getServiceIp() {
-        Enumeration<NetworkInterface> netInterfaces = null;
-        String ipsStr = "";
-        try {
-            netInterfaces = NetworkInterface.getNetworkInterfaces();
-            while (netInterfaces.hasMoreElements()) {
-                NetworkInterface ni = netInterfaces.nextElement();
-                Enumeration<InetAddress> ips = ni.getInetAddresses();
-                Pattern pattern = Pattern.compile(IP_PATTERN);
-                while (ips.hasMoreElements()) {
-                    String ip = ips.nextElement().getHostAddress();
-                    Matcher matcher = pattern.matcher(ip);
-                    if (matcher.matches() && !LOCAL_IP.equals(ip)) {
-                        ipsStr = ip;
-                    }
-                }
-            }
-        } catch (Exception e) {
-            logger.error("getServiceIp occurs error, caused by: ", e);
+    public static byte[] textToNumericFormatV4(String text)
+    {
+        if (text.length() == 0)
+        {
+            return null;
         }
 
-        return ipsStr;
+        byte[] bytes = new byte[4];
+        String[] elements = text.split("\\.", -1);
+        try
+        {
+            long l;
+            int i;
+            switch (elements.length)
+            {
+                case 1:
+                    l = Long.parseLong(elements[0]);
+                    if ((l < 0L) || (l > 4294967295L))
+                        return null;
+                    bytes[0] = (byte) (int) (l >> 24 & 0xFF);
+                    bytes[1] = (byte) (int) ((l & 0xFFFFFF) >> 16 & 0xFF);
+                    bytes[2] = (byte) (int) ((l & 0xFFFF) >> 8 & 0xFF);
+                    bytes[3] = (byte) (int) (l & 0xFF);
+                    break;
+                case 2:
+                    l = Integer.parseInt(elements[0]);
+                    if ((l < 0L) || (l > 255L))
+                        return null;
+                    bytes[0] = (byte) (int) (l & 0xFF);
+                    l = Integer.parseInt(elements[1]);
+                    if ((l < 0L) || (l > 16777215L))
+                        return null;
+                    bytes[1] = (byte) (int) (l >> 16 & 0xFF);
+                    bytes[2] = (byte) (int) ((l & 0xFFFF) >> 8 & 0xFF);
+                    bytes[3] = (byte) (int) (l & 0xFF);
+                    break;
+                case 3:
+                    for (i = 0; i < 2; ++i)
+                    {
+                        l = Integer.parseInt(elements[i]);
+                        if ((l < 0L) || (l > 255L))
+                            return null;
+                        bytes[i] = (byte) (int) (l & 0xFF);
+                    }
+                    l = Integer.parseInt(elements[2]);
+                    if ((l < 0L) || (l > 65535L))
+                        return null;
+                    bytes[2] = (byte) (int) (l >> 8 & 0xFF);
+                    bytes[3] = (byte) (int) (l & 0xFF);
+                    break;
+                case 4:
+                    for (i = 0; i < 4; ++i)
+                    {
+                        l = Integer.parseInt(elements[i]);
+                        if ((l < 0L) || (l > 255L))
+                            return null;
+                        bytes[i] = (byte) (int) (l & 0xFF);
+                    }
+                    break;
+                default:
+                    return null;
+            }
+        }
+        catch (NumberFormatException e)
+        {
+            return null;
+        }
+        return bytes;
+    }
+
+    public static String getHostIp()
+    {
+        try
+        {
+            return InetAddress.getLocalHost().getHostAddress();
+        }
+        catch (UnknownHostException e)
+        {
+        }
+        return "127.0.0.1";
+    }
+
+    public static String getHostName()
+    {
+        try
+        {
+            return InetAddress.getLocalHost().getHostName();
+        }
+        catch (UnknownHostException e)
+        {
+        }
+        return "未知";
     }
 }
