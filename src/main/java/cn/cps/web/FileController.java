0a1eec0bc79dea379604d2144316c83104b86f82
Mr丶Cps <1402333029@qq.com>
2019-09-12 11:29:04 +0800

    配置访问上传文件

--- src/main/java/cn/cps/web/FileController.java
+++ src/main/java/cn/cps/web/FileController.java
@@ -4,6 +4,7 @@ import cn.cps.core.Result;
 import cn.cps.core.ResultGenerator;
 import cn.cps.util.FileUtil;
 import com.alibaba.fastjson.JSON;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +23,8 @@ import java.io.*;
 @RequestMapping("/file")
 public class FileController {
 
-	String upLoadPath = "static"+File.separator+"upLoadFiles" + File.separator;
+	@Value("${web.upload-path}")
+	private String upLoadPath;
 
 	//如果不启用WebMvcConfigurer，不给类添加RequestMapping("/user")的话,下面就是配置默认访问页面
 	@GetMapping({"/", "/login.html"})
